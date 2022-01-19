3b420e13bedf4e01cdfe2a582178da72415655e6
Jouni Juntunen <jouni.juntunen@oamk.fi>
2022-01-19 10:39:54 +0200

    Added calculation.

--- App.js
+++ App.js
@@ -2,19 +2,37 @@ import { useState } from 'react';
 import { StyleSheet, Text, TextInput, View,Button } from 'react-native';
 
 export default function App() {
-  const [age, setAge] = useState(0);
+  const [age, setAge] = useState('');
   const [lower, setLower] = useState(0);
   const [upper, setUpper] = useState(0);
 
+  const calculate = () => {
+    const lowerLimit = (220-age) * 0.65;
+    setLower(lowerLimit);
+    const upperLimit = (220-age) * 0.85;
+    setUpper(upperLimit);
+  }
+
+/*   function calculate() {
+    const lowerLimit = (220-age) * 0.65;
+    setLower(lowerLimit);
+    const upperLimit = (220-age) * 0.85;
+    setUpper(upperLimit);
+  } */
+
+  
   return (
     <View style={styles.container}>
       <Text>Age</Text>
       <TextInput 
         keyboardType='numeric' 
+        placeholder='Enter age'
+        value={age}
+        onChangeText={setAge}
       />
       <Text>Limits</Text>
-      <Text></Text>
-      <Button title="Calculate" />
+      <Text>{lower.toFixed(0)} - {upper.toFixed(0)}</Text>
+      <Button onPress={calculate} title="Calculate" />
     </View>
   );
 }
