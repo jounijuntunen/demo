bd678a01cf8b80aa62a536e031a7e648be18c718
Jouni Juntunen <jouni.juntunen@oamk.fi>
2022-01-19 10:13:39 +0200

    Implemented UI for the app.

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View,Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Age</Text>
+      <TextInput 
+        keyboardType='numeric' 
+      />
+      <Text>Limits</Text>
+      <Text></Text>
+      <Button title="Calculate" />
     </View>
   );
 }
